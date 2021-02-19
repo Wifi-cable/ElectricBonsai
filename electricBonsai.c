d2ed02bbbf50476010634ac8bd1ab84fbfaaee09
wifi-cable <m.barsalou@stud.hs-mannheim.de>
2021-02-19 12:52:24 +0100

    faster flash speed,pwm initialized

--- electricBonsai.c
+++ electricBonsai.c
@@ -1,10 +1,23 @@
-#include  <avr/io.h>
-#include <avr/interrupt.h>
+#include <avr/io.h>
+
 
 
 void main( void){
+	//outputs
+	DDRB |= ((1 << DDB1) | (1 << DDB0)); 
 
- while(1){
- 
+	//pwm init ------------------
+	// Pin5 or red LED
+	TCCR0A |= (1 << COM0A1); 	//fast PWM
+	TCCR0A |= ((1 << WGM01) | (1 << WGM00)); //top = OCRA
+
+	// Pin5 or red LED
+	TCCR0B |= (1 << COM0B1); //fast PWM
+	TCCR0B |= (1 << WGM02) ; //top = OCRA
+	
+	//timer on 
+	TCCR0B |= (1 << CS00 );	//no prescaler initially
+	while(1){
+	
 	}
 }
--- makefile
+++ makefile
@@ -1,4 +1,4 @@
 all:
-	avr-gcc -DF_CPU=4800000UL -Os -mmcu=attiny13 electricBonsai.c -o electricBonsai.elf
+	avr-gcc -DF_CPU=1200000UL -Os -mmcu=attiny13 electricBonsai.c -o electricBonsai.elf
 	avr-objcopy -j .text -j .data -O ihex electricBonsai.elf electricBonsai.hex
 	avrdude -B 5 -p t13  -c avrispmkII -P usb -U flash:w:electricBonsai.hex
