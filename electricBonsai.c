493acac7ba504320dab518fbc60718b47a9cd93a
wifi-cable <m.barsalou@stud.hs-mannheim.de>
2021-03-09 20:10:26 +0100

    removed flawed logic in main loop, set propper prescaler

--- electricBonsai.c
+++ electricBonsai.c
@@ -1,25 +1,24 @@
 #include <avr/io.h>
-
+#include<avr/delay.h>
 void main( void){
 	//outputs
 	DDRB |= ((1 << DDB1) | (1 << DDB0)); 
 
 	//pwm init ------------------
 	// Pin5 or red LED
-	//TCCR0A |= (1 << COM0A1); 	//fast PWM 
-	//TCCR0A |= (1 << COM0A1) |(1 << COM0A0); 	//fast PWM Clear OC0A on Compare Match, set OC0A at TOP
-	TCCR0A |= (1 << COM0A0); 
+	TCCR0A |= (1 << COM0A1); 	//fast PWM 
+ 
 	
 	TCCR0A |= ((1 << WGM01) | (1 << WGM00)); //top = OCRA fast PWM
 	
 	TCCR0A |= (1 << COM0B1); 
 	// Pin5 or red LED
-	TCCR0B |= (1 << COM0B1); //fast PWM
+	TCCR0A |= (1 << COM0B1); //fast PWM
 	
 	//timer on 
 
-	TCCR0B |= (1 << WGM02); // wave form generation mode 7 
-	TCCR0B |= ((1 << CS02)| (1 << CS00));
+	
+	TCCR0B |= ((0 << CS02)| (1 << CS00));  // /64
 	
 	uint8_t blue = 0;
 	uint8_t pink = 253; 
@@ -27,54 +26,20 @@ void main( void){
 	uint8_t blue_fade = 1; 
 	
 	uint8_t prescale = 0;
-	
-	
-	
+
+OCR0A = 128;
+
 		while(1){
+		/**TODO
+		setup slow timer interrupt, conter and flag
+		fade LEDs 
 		
-		
-		//if(prescale >= 253){
-		
-			if(pink_fade){
-				pink--;
-				blue--;
-			}
-			else{
-				pink++; 
-				blue--;
-			}
-			
-			if(blue_fade){
-				blue--;
-			}
-			else{
-			 blue++; 
-			}
-		//prescale = 0;
-		//}
-		/**
-		else{
-			prescale ++; 
-		}
-		**/
-		if(pink >= 253){	// decrease
-			pink_fade = 1;
-		}
-		
-		else if(pink <= 1){	// increase 
-			pink_fade = 0;
-		}
-		
-		else if(blue >= 253){	//decrease
-			blue_fade = 1;
-		}
-		
-		else if(blue <= 1){	// increase
-			blue_fade = 0;
-		}
-		
-		OCR0A = blue;
-		OCR0B = pink;
+		Maybe look for formula or arrayy with logarythmic fadees
+**/
+		_delay_ms(20);
+		OCR0A++;
+		OCR0B--;
+}
 	
-		}
+
 }
